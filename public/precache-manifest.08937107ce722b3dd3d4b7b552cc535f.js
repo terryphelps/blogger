88d84f192bbe7880fc6344392fa7aa69caea64f0
Jake Overall <joverall22@gmail.com>
2019-07-11 16:08:26 -0600

    Add files via upload

---
+++ public/precache-manifest.08937107ce722b3dd3d4b7b552cc535f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "555aef7441004d6cb7ce",
+    "url": "/css/app.889365d7.css"
+  },
+  {
+    "revision": "555aef7441004d6cb7ce",
+    "url": "/js/app.b3b2f3c4.js"
+  },
+  {
+    "revision": "e1236b7e5ebb5fdd3dd4",
+    "url": "/js/chunk-vendors.eebc5769.js"
+  },
+  {
+    "revision": "79285fa8d0cbbde32fddd5ac579320ef",
+    "url": "/img/banner.79285fa8.png"
+  },
+  {
+    "revision": "99e135dd034b7550bc0fc662af09e3d2",
+    "url": "/img/blogger-logo.99e135dd.png"
+  },
+  {
+    "revision": "b8dc3badbe60be89f4315b062b16ba58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951a67a8a6d9e122fd4257c4b90a0fab",
+    "url": "/favicon-blogger.ico"
+  },
+  {
+    "revision": "0d3c1f4a097eaad301a1a795a555438a",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
